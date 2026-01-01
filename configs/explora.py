6d008e72eacb5b1c8b3b323c27fbba0beb5cabb4
F. Burcak Duruoz <bduruoz@gmail.com>
2026-01-02 01:49:19 +0300

    Update

--- configs/explora.py
+++ configs/explora.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(r"E:\AI-Outputs\LoraWeights\Publish")
 PUBLISHED_DIR = BASE_DIR / "Published"
 EXCEL_PATH = BASE_DIR / "expLora_registry.xlsx"
 
-# istediğin renk paleti
+# Color Palette
 HEADER_COLOR = "#D9E1F2"
 TRUE_COLOR   = "#7DEF94"
 FALSE_COLOR  = "#F06979"
@@ -13,9 +13,9 @@ ERROR_COLOR  = "#FFBDC7"
 ZEBRA_DARK   = "#E8E8E8"
 
 LM_STUDIO_URL = "http://localhost:9090/v1"
-LM_MODEL_NAME = "openai/gpt-oss-120b"   # kurulu model
-SEO_KEYWORDS  = ["AI art", "stable diffusion", "lora"]
-COMMON_TAGS   = ["#AIart", "#StableDiffusion", "#Lora"]
+LM_MODEL_NAME = "openai/gpt-oss-120b"   # used model
+SEO_KEYWORDS  = ["AI art", "ComfyUI", "LoRA", "Stable Diffusion", "AI generated art", "AI illustrations", "AI creativity", "AI image generation", "SDXL", "Dream Shaper XL"]
+COMMON_TAGS   = ["#AIart", "#ComfyUI", "#LoRA", "#StableDiffusion", "#AIGeneratedArt", "#AIillustration", "#AIcreativity", "#AIimageGeneration", "#SDXL", "#DreamShaperXL"]
 INCLUDE_LINKS = True
 LINKS = ["https://duruoz.net/watch/{set_name}",
          "https://duruoz.net/blog/{set_name}"]
